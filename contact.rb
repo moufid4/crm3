0f3976e513647061e68865bccb6ebd1c715b9346
Moufid Larbi <moufid4@gmail.com>
2014-08-22 21:14:01 -0400

    first commit

---
+++ contact.rb
@@ -0,0 +1,18 @@
+class Contact
+	attr_accessor :first_name, :last_name, :email, :notes
+
+	def initialize
+	# As a developer, the contact class should have five attributes: id, first_name, last_name, email and notes.
+	# As a developer, when a user creates a contact, all five attributes are mandatory.
+	end
+
+	def diplay_all_values
+		
+	end
+end
+
+
+
+
+# As a developer, the Contact class should also have a method that displays all of its values.
+
---
+++ rolodex.rb
@@ -0,0 +1,32 @@
+class Rolodex
+
+	def initialize
+		# As a developer, I should implement the Rolodex class as an array that accepts new contacts.
+
+	end
+	
+	def add
+
+	end
+
+	def modify_contact
+		
+	end
+
+	def display_all_contacts
+		
+	end
+
+	def display_particular_contact
+		
+	end
+
+	def display_info_by_attribute
+		
+	end
+
+	def delete_contact
+		
+	end
+
+end
